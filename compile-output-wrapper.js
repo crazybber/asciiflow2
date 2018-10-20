3e849550f957533a7de778522c34f097f2e0e589
LMLB <LMLB@users.noreply.github.com>
2018-10-20 23:02:01 +0200

    Make Closure Compiler output source map

---
+++ compile-output-wrapper.js
@@ -0,0 +1,2 @@
+(function(){%output%})();
+//# sourceMappingURL=js-compiled.js.map
--- compile.bat
+++ compile.bat
@@ -2,5 +2,5 @@
 
 :: Removed --formatting=PRETTY_PRINT
 
-java -client -jar closure-compiler.jar --js js-lib\*.js --js js-lib\*\*.js --warning_level=VERBOSE --language_in=ECMASCRIPT6 --compilation_level=ADVANCED_OPTIMIZATIONS --output_wrapper "(function() {%%output%%})();" > js-compiled.js
+java -client -jar closure-compiler.jar --create_source_map js-compiled.js.map --js_output_file js-compiled.js --js js-lib\*.js --js js-lib\*\*.js --warning_level=VERBOSE --language_in=ECMASCRIPT6 --compilation_level=ADVANCED_OPTIMIZATIONS --output_wrapper_file compile-output-wrapper.js
 pause
